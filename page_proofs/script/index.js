ed24b4d9a7bcdc31e3049dab55394de0c99bf33a
Vlad Korochanskiy <mginster234942@gmai.com>
2022-11-01 21:29:21 +0200

    create 3 section

--- page_proofs/script/index.js
+++ page_proofs/script/index.js
@@ -1,6 +1,12 @@
 const burger = document.querySelector(".burger");
 const navBar = document.querySelector(".nav_menu");
 
+const navbars = document.querySelectorAll(".active_burger .nav_menu_item");
+const navMenuItems = document.querySelector(".nav_menu_item");
+const subNavMenuLists = document.querySelector(".sub_nav_menu_list");
+const activeBurger = document.querySelector(".active_burger");
+console.log(navbars);
+
 burger.addEventListener("click", () => {
   if (navBar.classList.contains("nav_menu")) {
     navBar.classList.add("active_burger");
@@ -11,6 +17,12 @@ burger.addEventListener("click", () => {
   }
 });
 
-// document.querySelector(".burger").onclick = function () {
-//   document.querySelector(".nav_menu").classList.add("active_burger");
-// };
+navMenuItems.addEventListener("click", () => {
+  if (subNavMenuLists.classList.contains("sub_nav_menu_list")) {
+    subNavMenuLists.classList.add("none");
+    subNavMenuLists.classList.remove("sub_nav_menu_list");
+  } else {
+    subNavMenuLists.classList.add("sub_nav_menu_list");
+    subNavMenuLists.classList.add("none");
+  }
+});
